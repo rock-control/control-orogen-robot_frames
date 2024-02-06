7051af7fb3eb153b1e22c206009a0720dbb57f0f
Ravisankar Selvaraju <ravisankar.selvaraju@dfki.de>
2024-02-06 14:59:42 +0000

    test moving functionality to library

--- tasks/ChainPublisher.hpp
+++ tasks/ChainPublisher.hpp
@@ -4,7 +4,6 @@
 #define ROBOT_FRAMES_CHAINPUBLISHER_TASK_HPP
 
 #include "robot_frames/ChainPublisherBase.hpp"
-#include "robot_framesTypes.hpp"
 #include <kdl_parser/kdl_parser.hpp>
 #include <kdl/chainfksolvervel_recursive.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
@@ -21,22 +20,8 @@ class ChainPublisher : public ChainPublisherBase
 {
     friend class ChainPublisherBase;
 protected:
-    std::vector<KDL::Chain> chains_;
-    std::vector<std::string> chain_names_;
-    std::vector<KDL::ChainFkSolverPos_recursive*> pos_solvers_;
-    std::vector<KDL::Frame> kdl_frames_;
-    std::vector<base::samples::RigidBodyState> bt_frames_;
-    std::vector<KDL::JntArray> joint_arrays_;
-    std::vector<std::vector<std::string> >involved_active_joints_;
-    base::samples::Joints joint_state_;
-    KDL::Tree tree_;
-    int n_defined_chains_;
-    std::vector<RTT::OutputPort<base::samples::RigidBodyState>*> out_ports_;
-
-    void clear_and_resize_vectors();
-    bool unpack_joints(const base::samples::Joints& joint_state,
-                       const std::vector<std::string>& involved_joints,
-                       KDL::JntArray& joint_array);
+     base::samples::Joints joint_state;
+     std::vector<RTT::OutputPort<base::samples::RigidBodyState>*> out_ports_;
 
 public:
     ChainPublisher(std::string const& name = "robot_frames::ChainPublisher");
