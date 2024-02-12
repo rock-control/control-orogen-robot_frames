8b5004ff2567c0c61a6909ea3cfe33c7870f28fe
Ravisankar <91879299+RavisankarSelvaraju@users.noreply.github.com>
2024-02-12 10:55:33 +0100

    Move parts of ChainPublisher's implementation from orogen component to lib (#6)
    * test moving functionality to library
    
    * modify .orogen file to point to library for robot_frameTypes
    
    * simplify component code for updated robot_frames library
    
    * bug fix
    fixed a pointer and deleting the pointer at cleanup
    modify variable name
    fix loop iteration
    
    * clear ports and vector for every configuration
    
    ---------
    
    Authored-by: Ravisankar Selvaraju <ravisankar.selvaraju@dfki.de>

--- tasks/ChainPublisher.hpp
+++ tasks/ChainPublisher.hpp
@@ -4,13 +4,12 @@
 #define ROBOT_FRAMES_CHAINPUBLISHER_TASK_HPP
 
 #include "robot_frames/ChainPublisherBase.hpp"
-#include "robot_framesTypes.hpp"
 #include <kdl_parser/kdl_parser.hpp>
 #include <kdl/chainfksolvervel_recursive.hpp>
 #include <kdl/chainfksolverpos_recursive.hpp>
 #include <kdl/jntarray.hpp>
 #include <base/samples/RigidBodyState.hpp>
-#include <base/Logging.hpp>
+#include <base-logging/Logging.hpp>
 
 #include <robot_frames/RobotFrames.hpp>
 
@@ -21,26 +20,14 @@ class ChainPublisher : public ChainPublisherBase
 {
     friend class ChainPublisherBase;
 protected:
-    std::vector<KDL::Chain> chains_;
-    std::vector<std::string> chain_names_;
-    std::vector<KDL::ChainFkSolverPos_recursive*> pos_solvers_;
-    std::vector<KDL::Frame> kdl_frames_;
-    std::vector<base::samples::RigidBodyState> bt_frames_;
-    std::vector<KDL::JntArray> joint_arrays_;
-    std::vector<std::vector<std::string> >involved_active_joints_;
-    base::samples::Joints joint_state_;
-    KDL::Tree tree_;
-    int n_defined_chains_;
-    std::vector<RTT::OutputPort<base::samples::RigidBodyState>*> out_ports_;
+     base::samples::Joints joint_state;
+     std::vector<base::samples::RigidBodyState> bt_frames_;
+     std::vector<RTT::OutputPort<base::samples::RigidBodyState>*> output_ports_;
 
-    void clear_and_resize_vectors();
-    bool unpack_joints(const base::samples::Joints& joint_state,
-                       const std::vector<std::string>& involved_joints,
-                       KDL::JntArray& joint_array);
+     ChainTransformationCalculator* chain_transformer;
 
 public:
     ChainPublisher(std::string const& name = "robot_frames::ChainPublisher");
-
     ChainPublisher(std::string const& name, RTT::ExecutionEngine* engine);
 
     ~ChainPublisher();
